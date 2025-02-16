f084a794955bfa437b94fe5529e6887057fa5ba6
1ris_W <ediath462@gmail.com>
2025-02-16 13:15:34 +0800

    Initial Commit

---
+++ PIP_Demo/PIP_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIP_DemoApp.swift
+//  PIP_Demo
+//
+//  Created by Iris on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct PIP_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
